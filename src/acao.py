c35178eef52002788205c44610e247cde06af096
Luiz Henrique <lhmaia@gmail.com>
2016-04-07 23:06:55 -0300

    lendo arquivo

--- src/acao.py
+++ src/acao.py
@@ -1,23 +1,26 @@
 #codigo,fechamento_atual,abertura,maximo,minimo,fechamento_anterior,negocios,quantidade_papeis,volume_financeiro,datahora
 class Candle:
     '''classe contendo os valores de um candle'''
-    fechamento_atual = 0.0
-    abertura = 0.0
-    maximo = 0.0
-    minimo = 0.0
-    fechamento_anterior = 0.0
-    negocios = 0
-    quantidade_papeis = 0
-    volume_financeiro = 0.0
-    datahora = None
 
+    def __init__(self):
+        self.fechamento_atual = 0.0
+        self.abertura = 0.0
+        self.maximo = 0.0
+        self.minimo = 0.0
+        self.fechamento_anterior = 0.0
+        self.negocios = 0
+        self.quantidade_papeis = 0
+        self.volume_financeiro = 0.0
+        self.datahora = None
+
+    def __str__(self):
+        return str(self.fechamento_atual) + ',' + str(self.abertura) + ',' +  str(self.maximo) + ',' + str(self.minimo) + ',' + str(self.fechamento_anterior) + ',' + str(self.negocios) + ',' + str(self.quantidade_papeis) + ',' + str(self.volume_financeiro) + ',' + str(self.datahora) 
 
 class Acao:
     '''classe representando uma acao '''
-    codigo = ''
-    candles = []
     def __init__(self, codigo):
         self.codigo = codigo
+        self.candles = []
 
     def __eq__(self, other):
         return self.codigo == other.codigo
--- src/ler_dados.py
+++ src/ler_dados.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 from acao import Acao, Candle
+from datetime import datetime
 
-f = open('../Dados/20150725_candles_ibov_15min_10campos_comCabecalho.txt', 'r')
+#f = open('../Dados/20150725_candles_ibov_15min_10campos_comCabecalho.txt', 'r')
+f = open('../Dados/dadosTrabalho.txt', 'r')
 
 acoes = []
 
@@ -9,16 +11,36 @@ acoes = []
 f.readline()
 
 for linha in f:
-    acao = linha[0: linha.find(',')]
-    a = Acao(acao)
+    tmp = linha.split(',') #[0: linha.find(',')]
+    a = Acao(tmp[0])
+    
+    c = Candle()
+    c.fechamento_atual = tmp[1]
+    c.abertura = tmp[2]
+    c.maximo = tmp[3]
+    c.minimo = tmp[4]
+    c.fechamento_anterior = tmp[5]
+    c.negocios = tmp[6]
+    c.quantidade_papeis = tmp[7]
+    c.volume_financeiro = tmp[8]
+    c.datahora = datetime.strptime(tmp[9][:-1], '%Y%m%d%H%M')
+
+
     if not a in acoes:
         acoes.append(a)
+        acoes[acoes.index(a)].candles.append(c)
+    else:
+        acoes[acoes.index(a)].candles.append(c)
+
 
 print 'Numero de acoes: ' + str(len(acoes))
 
-acoes.sort(cmp=lambda x,y: cmp(x.codigo, y.codigo))
+#acoes.sort(cmp=lambda x,y: cmp(x.codigo, y.codigo))
 
 for acao in acoes:
-    print acao.codigo
+    print acao.codigo + ', numero de candles: ' + str(len(acao.candles))
+
+for i in range(20):
+    print acoes[0].candles[i]
 
 f.close()
